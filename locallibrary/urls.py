0661969b08a2b9515869a4c42dbe5736c64cb2a2
Jitendra Kumar <jitendrar048@gmail.com>
2020-11-16 17:24:27 +0530

    add django site authentication urls (for login, logout, password management)

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -27,3 +27,7 @@ urlpatterns = [
 
 # urlpatterns+=[path('', RedirectView.as_view(url='catalog/', permanent=True)),]
 # urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+urlpatterns +=[
+    path('accounts/',include('django.contrib.auth.urls')),
+]
