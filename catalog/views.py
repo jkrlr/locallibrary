87f788ca42216543c751e2c82e30bf581160eeeb
Jitendra Kumar <jitendrar048@gmail.com>
2020-11-17 16:25:15 +0530

    add view for loan Books of a user

--- catalog/views.py
+++ catalog/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from catalog.models import Book, Author, BookInstance, Genre
 from django.views import generic
 from django.shortcuts import get_object_or_404
-
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 
 
@@ -75,3 +75,18 @@ class AuthorDetailView(generic.DetailView):
 def author_detail_view(request,primary_key):
     author=get_object_or_404(Author,pk=primary_key)
     return render(request,'catalog/author_detail.html',context={'author':author})
+
+
+
+
+# On loan View of books for a user
+
+class LoanedBooksByUserListView(LoginRequiredMixin,generic.ListView):
+    """Generic class-based view listing books on loan to current user."""
+    model=BookInstance
+    template_name='catalog/bookinstance_list_borrowed_user.html'
+    paginated_by=10
+
+    def get_queryset(self):
+        return  BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
+    
