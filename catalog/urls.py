d6fbc80607f77296da153c10e08a70af48af35e7
Jitendra Kumar <jitendrar048@gmail.com>
2020-11-23 19:00:15 +0530

    add URL configuration for renew-books page

--- catalog/urls.py
+++ catalog/urls.py
@@ -15,3 +15,8 @@ urlpatterns += [
     path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
     path('borrowed/', views.LoanedBooksAllListView.as_view(), name='all-borrowed'),
 ]
+
+# add URL configuration for renew-books page
+urlpatterns+=[
+    path('book/<uuid:pk>/renew/', views.renew_book_librarian, name='renew-book-librarian'),
+]
