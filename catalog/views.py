3fd9153d949154b9bf24c5c3668d27e5af73485d
Jitendra Kumar <jitendrar048@gmail.com>
2020-11-24 10:14:02 +0530

    add Create, Update, Delete view of Author

--- catalog/views.py
+++ catalog/views.py
@@ -10,6 +10,8 @@ from django.contrib.auth.decorators import permission_required
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from catalog.forms import RenewBookForm
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 # Create your views here.
 
 
@@ -142,4 +144,20 @@ def renew_book_librarian(request, pk):
     }
 
     return render(request,'catalog/book_renew_librarian.html', context)
-           
+
+
+
+# generic editing view(create, update, delete) of Author
+
+class AuthorCreate(CreateView):
+    model=Author
+    fields='__all__'
+    initial={'date_of_death': '08/04/2025'}
+
+class AuthorUpdate(UpdateView):
+    model=Author
+    fields=['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+
+class AuthorDelete(DeleteView):
+    model=Author
+    success_url=reverse_lazy('authors')
